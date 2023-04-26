0e400108f9c8ceb71a5f5edb4d10fc93905fd2e3
Amanda Matar <amandamatar@gmail.com>
2023-04-26 15:49:46 -0300

    commits aulas 1 e 2

--- src/main/kotlin/me/dio/credit/application/system/CreditRepository.kt
+++ src/main/kotlin/me/dio/credit/application/system/CreditRepository.kt
@@ -1,4 +1,8 @@
 package me.dio.credit.application.system
 
-interface CreditRepository {
-}
+import me.dio.credit.application.system.entity.Credit
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface CreditRepository: JpaRepository<Credit, Long>
